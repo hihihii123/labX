a133b6443faad0a031e75c0e8bb8e946949cced0
indigoX234 <parthasarathy_dhanush@s2023.ssts.edu.sg>
2024-07-22 15:41:45 +0800

    added the files for ISS>Resources

---
+++ citation.js
@@ -0,0 +1,12 @@
+import { View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState } from 'react';
+
+export default function LabBookPage({}) {
+    return (
+        <View style={{flex:1, alignItems: 'center', justifyContent: 'center', }}>
+            <Text>Placeholder for citation page</Text>
+        </View>
+    );
+  }
